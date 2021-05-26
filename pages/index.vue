33ace65bba82be143e6397b14fb09871883992d9
PJH <icecrea4@naver.com>
2021-05-26 11:47:01 +0900

    add nav & footer

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
           간단한 생활요리<i class="fas fa-drumstick-bite"></i>
         </p>
         <p class="subtitle">
-          <i class="fas fa-home"></i>집에서 간단히 해 먹을 수 있는 생활 요리 레시피<i class="fas fa-scroll"></i>
+          <i class="fas fa-house-user"></i>집에서 간단히 해 먹을 수 있는 생활 요리 레시피<i class="fas fa-scroll"></i>
         </p>
       </div>
     </section>
