129e2dcde1b29ca17b974ad0e6747c94171150cb
PJH <icecrea4@naver.com>
2021-06-22 22:30:26 +0900

    [Bug] Eggfried-ricewithsmokeyflavorMaterials.json

--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "[Bug] eggfried-ricewithsmokeyflavorMaterials.json"
+git commit -m "[Bug] Eggfried-ricewithsmokeyflavorMaterials.json"
 git push -u origin master
